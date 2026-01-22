20652187d94a9acd6daf57f9c89ddb9305017fbd
Lee Dong Seok <dongseok3103@gmail.com>
2026-01-22 15:21:35 +0900

    홈제이지 1차 구성

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://MooseDS.github.io',
-    base: '/',
+  site: 'https://MooseDS.github.io',
+  base: '/',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
